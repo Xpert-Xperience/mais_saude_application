68f5687623b8208b9d6e0b67c7308259153931b1
PRoberto04 <paulo.rcarlos23@gmail.com>
2024-03-23 01:31:21 -0300

    Arquivo 'confesq' alterado para 'confirmar_sennha_view'

---
+++ lib/view/esqueceu_senha/confirmar_senha_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ConfirmarEsqueceuSenha extends StatefulWidget {
+  const ConfirmarEsqueceuSenha({super.key});
+
+  @override
+  State<ConfirmarEsqueceuSenha> createState() => _ConfirmarEsqueceuSenhaState();
+}
+
+class _ConfirmarEsqueceuSenhaState extends State<ConfirmarEsqueceuSenha> {
+  @override
+  Widget build(BuildContext context) {
+     return Container(
+      color: const Color.fromARGB(255, 140, 18, 18),
+    );
+  }
+}
