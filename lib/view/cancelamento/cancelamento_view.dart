22018109cd55836beb178f242cbffb0f77b7c82f
Laraagr1 <laraagrela94@gmail.com>
2024-03-31 19:53:09 -0300

    construção da tela de cancelamento de consultas

--- lib/view/cancelamento/cancelamento_view.dart
+++ lib/view/cancelamento/cancelamento_view.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:mais_saude/view/historico/historico_view.dart';
 
 class Cancelamento extends StatefulWidget {
   const Cancelamento({super.key});
 
   @override
   State<Cancelamento> createState() => _CancelamentoState();
+}
+
+double displayWidth(BuildContext context) {
+  return MediaQuery.of(context).size.width;
 }
 
 class _CancelamentoState extends State<Cancelamento> {
@@ -26,8 +31,79 @@ class _CancelamentoState extends State<Cancelamento> {
             },
           ),
         ),
-        title: Text('Cancelamento', style: TextStyle(fontSize: 25)),
+        title: Text(
+          'Cancelamento',
+          style: TextStyle(fontSize: 25),
+        ),
+        centerTitle: true,
         foregroundColor: Color.fromARGB(255, 255, 255, 255),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: 50),
+            Text(
+              'Cancelar',
+              style: TextStyle(
+                fontSize: 36,
+                fontWeight: FontWeight.bold,
+                color: Color(0xFF0D4542),
+              ),
+            ),
+            Text(
+              'Agendamento',
+              style: TextStyle(
+                  fontSize: 36,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xFF0D4542)),
+            ),
+            SizedBox(height: 18),
+            Text(
+              'Tem certeza que deseja CANCELAR esse agendamento? Essa ação não poderá ser desfeita.',
+              style: TextStyle(fontSize: 14, color: Color(0xFF014B47)),
+            ),
+            const SizedBox(height: 110),
+            Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const Historico()),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                    minimumSize: Size(displayWidth(context) * 0.8, 55),
+                    backgroundColor: Color(0xFF0A9080)),
+                child: const Text(
+                  'Confirmar',
+                  style: TextStyle(
+                      fontSize: 22, color: Color.fromARGB(255, 255, 255, 255)),
+                ),
+              ),
+            ),
+            const SizedBox(height: 60),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const Historico()),
+                );
+              },
+              child: Align(
+                alignment: Alignment.center,
+                child: Text(
+                  'Manter',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Color(0xFF014B47),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
